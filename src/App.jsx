4621fd56959134d965c6802d461d721a9f75233a
juan manuel <jbolivarm2@ucentral.edu.co>
2026-05-17 19:01:54 -0500

    flex-direction-column movil

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,21 @@ import { Link } from "react-router-dom";
 import { Board } from "./components/Board";
 import { Marcador } from "./components/Marcador";
 import { TicTacToeAI } from "./components/TicTacToeAi";
-function App({ticAi}) {
+function App({ ticAi }) {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
   const xIsNext = currentMove % 2 === 0;
   const [score, setScore] = useState(() => {
-  try {
-    const saved = localStorage.getItem("data");
-    // Si existe y no es "undefined", parsealo. Si no, usa el default.
-    return saved ? JSON.parse(saved) : { x: 0, o: 0 };
-  } catch (error) {
-    return { x: 0, o: 0 };
-  }
-});
+    try {
+      const saved = localStorage.getItem("data");
+      // Si existe y no es "undefined", parsealo. Si no, usa el default.
+      return saved ? JSON.parse(saved) : { x: 0, o: 0 };
+    } catch (error) {
+      return { x: 0, o: 0 };
+    }
+  });
   const [gameOver, setGameOver] = useState(false);
   let currentSquares = history[currentMove];
-  
 
   function handlePlay(nexSquares) {
     const nexHistory = [...history.slice(0, currentMove + 1), nexSquares];
@@ -33,61 +32,63 @@ function App({ticAi}) {
     setGameOver(false);
     setCurrentMove(0);
   };
-  const borrarJuego = ()=>{
+  const borrarJuego = () => {
     reiniciarJuego();
-    setScore({x:0, o:0});
+    setScore({ x: 0, o: 0 });
     localStorage.setItem("data", { x: 0, o: 0 });
-  }
+  };
   function handleWin(winner) {
-  if (gameOver || !winner) return; 
+    if (gameOver || !winner) return;
 
-  setScore((prev) => {
-    // Si por alguna razón prev es null, lo inicializamos aquí
-    const currentScore = prev || { x: 0, o: 0 };
-    const key = winner.toLowerCase();
+    setScore((prev) => {
+      // Si por alguna razón prev es null, lo inicializamos aquí
+      const currentScore = prev || { x: 0, o: 0 };
+      const key = winner.toLowerCase();
 
-    return {
-      ...currentScore,
-      [key]: (currentScore[key] || 0) + 1,
-    };
-  });
+      return {
+        ...currentScore,
+        [key]: (currentScore[key] || 0) + 1,
+      };
+    });
 
-  setGameOver(true);
-}
+    setGameOver(true);
+  }
 
   // ACTUALIZAR LOCALSTORAGE
-  useEffect(()=>{
+  useEffect(() => {
     localStorage.setItem("data", JSON.stringify(score));
-  }, [score])
+  }, [score]);
 
   return (
     <div className="slide-left">
-    <div className="game">
-      <Marcador props={score} />
-      <div className="game-board">
-        <Board
-          xIsNext={xIsNext}
-          squares={currentSquares}
-          onplay={handlePlay}
-          onWinner={handleWin}
-        />
+      <div className="game">
+        <Marcador props={score} />
+        <div className="game-board">
+          <Board
+            xIsNext={xIsNext}
+            squares={currentSquares}
+            onplay={handlePlay}
+            onWinner={handleWin}
+          />
+        </div>
+        <div className="menu-juego">
+          {ticAi && (
+            <TicTacToeAI
+              xIsNext={xIsNext}
+              squares={currentSquares}
+              onPlay={handlePlay}
+              gameOver={gameOver}
+            />
+          )}
+          <div className="button-container">
+            <Link to="/history" state={{ history, currentMove }}>
+              <button>Ir a historial</button>
+            </Link>
+            <button onClick={() => reiniciarJuego()}>Jugar Otra Vez</button>
+            <button onClick={() => borrarJuego()}>Reiniciar</button>
+          </div>
+        </div>
       </div>
-      <div className="menu-juego">
-        {ticAi &&
-          <TicTacToeAI 
-          xIsNext={xIsNext} 
-          squares={currentSquares} 
-          onPlay={handlePlay} 
-          gameOver={gameOver} 
-        />
-        }
-        <Link to="/history" state={{ history, currentMove }}>
-        <button>Ir a historial</button>
-      </Link>
-      <button onClick={() => reiniciarJuego()}>Jugar Otra Vez</button>
-      <button onClick={() => borrarJuego()}>Reiniciar</button>
-      </div>
-    </div>
     </div>
   );
 }
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,19 +1,17 @@
-
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { HistorialGame } from "../pages/HistorialGame";
 import { Player_1 } from "../components/Player_1";
 import App from "../App";
 import { Menu } from "../components/Menu";
-export function Routers(){
-    return(
-        <Router>
-            <Routes>
-                <Route path="/" element={<Menu/>}  />
-                <Route path="/history" element={<HistorialGame/>} />
-                <Route path="/2-player" element={<App ticAi={false}/>} />
-                <Route path="/1-player" element={<Player_1/>} />
-            </Routes>
-        </Router>
-    );
+export function Routers() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Menu />} />
+        <Route path="/history" element={<HistorialGame />} />
+        <Route path="/2-player" element={<App ticAi={false} />} />
+        <Route path="/1-player" element={<Player_1 />} />
+      </Routes>
+    </Router>
+  );
 }
